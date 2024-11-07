a59403578d5151b197d9d4ba4d4405ff7824b90d
Joshua13679 <joshuamking13679@gmail.com>
2024-11-07 14:55:58 -0500

    Add Start Button
    Added Start Button

--- BucsEyeView/js/main.js
+++ BucsEyeView/js/main.js
@@ -11,3 +11,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Start button object
+const StartButton = document.getElementById("startButton");
+
+// Game canvas object
+const canvas = document.getElementById('gameCanvas');
+
+// Game canvas context
+const ctx = canvas.getContext('2d');
+
+// Starts game when called
+startButton.addEventListener("click", function() {
+    // Game Code goes here
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    startButton.style.display = 'none';
+});
