31e8a05b1bc4c0e0895575bd38a3f69b016cf64d
Grace <123522499+gmc9193@users.noreply.github.com>
2024-11-07 14:00:41 -0500

    Add files via upload

---
+++ BucsEyeView/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    alert("UwU");
+};
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
