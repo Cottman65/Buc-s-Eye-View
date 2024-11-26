0440a843297722c0b7e78b32b26ce80ae2045445
Joshua13679 <joshuamking13679@gmail.com>
2024-11-25 19:53:40 -0500

    Score logic
    Adds logic for the score system

---
+++ Buc-s-Eye-View/BucsEyeView/js/main.js
@@ -0,0 +1,13 @@
+/*
+ *
+ *  Main Javascript page that runs all other logic
+ * 
+ */
+
+// Call this function to initialize the menu and display the start game
+drawMenu();
+
+// Event listener for canvas clicks
+canvas.addEventListener("click", handleCanvasClick);
+
+
